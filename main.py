95d9d21a0602c43baf7a73d649d24d80988d84a5
German Ivanov <germanivanov0719@gmail.com>
2021-10-30 15:23:21 +0300

    0.1.0
    - Fernet Encryption/Decryption and key generation now works fine
    - Added buttons for copy and paste (inactive now)
    - More message boxes
    - import pyperclip
    - code became more clean and straightforward

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import hashlib
 import sys
 from cryptography.fernet import Fernet
 import hashlib
+import pyperclip as pc
 
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, QMessageBox
@@ -11,10 +12,50 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('design.ui', self)
+
+        # Connecting elements
+
+        # Fernet Key Generators
         self.generate_fe.clicked.connect(self.fgenerate_fe)
+        self.generate_fd.clicked.connect(self.fgenerate_fd)
+
+        # Fernet Encrypt/Decrypt
+        self.encrypt_fe.clicked.connect(self.fencrypt_fe)
+        self.decrypt_fd.clicked.connect(self.fdecrypt_fd)
+
+        # Copy and Paste buttons
+        # self.copy_fe.clicked.connect()
+
+    def fencrypt_fe(self):
+        f = 0
+        try:
+            f = Fernet(self.key_fe.text().encode())
+        except ValueError:
+            wa = QMessageBox()
+            wa.setText("The key you entered is not a valid Fernet key")
+            wa.setInformativeText("You can generate a new valid key by pressing \"Generate\"")
+            wa.setStandardButtons(QMessageBox.Ok)
+            wa.exec_()
+            return 0
+        encrypted = f.encrypt(self.textEdit_fe.toPlainText().encode())
+        self.result_fe.setText(encrypted.decode())
+
+    def fdecrypt_fd(self):
+        f = 0
+        try:
+            f = Fernet(self.key_fd.text().encode())
+        except ValueError:
+            wa = QMessageBox()
+            wa.setText("The key you entered is not a valid Fernet key")
+            wa.setInformativeText("You can generate a new valid key by pressing \"Generate\"")
+            wa.setStandardButtons(QMessageBox.Ok)
+            wa.exec_()
+            return 0
+        decrypted = f.decrypt(self.result_fd.text().encode())
+        self.textEdit_fd.setText(decrypted.decode())
+
 
     def fgenerate_fe(self):
-        hashlib.md5()
         wa = QMessageBox()
         wa.setText("You are about to generate a new Fernet key. ")
         wa.setInformativeText("The new key is going to replace the old one. Are you sure you can safely proceed?")
@@ -22,6 +63,15 @@ class MainWindow(QMainWindow):
         # wa.setDefaultButton(QMessageBox.No)
         if wa.exec() == QMessageBox.Yes:
             self.key_fe.setText(Fernet.generate_key().decode())
+
+    def fgenerate_fd(self):
+        wa = QMessageBox()
+        wa.setText("You are about to generate a new Fernet key. ")
+        wa.setInformativeText("The new key is going to replace the old one. Are you sure you can safely proceed?")
+        wa.setStandardButtons(QMessageBox.No | QMessageBox.Yes)
+        # wa.setDefaultButton(QMessageBox.No)
+        if wa.exec() == QMessageBox.Yes:
+            self.key_fd.setText(Fernet.generate_key().decode())
 
 
     def generate_fernet_key(self):
