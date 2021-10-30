9581538e24b6aa6c0774c7e2aa580e341d208e28
German Ivanov <germanivanov0719@gmail.com>
2021-10-30 21:44:14 +0300

    0.3
    - Added SHA support
    Note: No protections for SHA tab yet

--- main.py
+++ main.py
@@ -31,6 +31,39 @@ class MainWindow(QMainWindow):
         self.paste_fe.clicked.connect(self.fpaste_fe)
         self.paste_fd.clicked.connect(self.fpaste_fd)
 
+        # Sha hashing
+        self.hash_sha.clicked.connect(self.fhash_sha)
+
+    def fhash_sha(self):
+        text = self.textEdit_sha.toPlainText().encode()
+        alg = self.sha_btn_group.checkedButton().text().lower().replace('sha', '').replace('-', '')
+        # print(alg)
+        r = ''
+        if alg == '1':
+            r = hashlib.sha1(text)
+        # SHA-2 family
+        elif alg == '256':
+            r = hashlib.sha256(text)
+        elif alg == '224':
+            r = hashlib.sha224(text)
+        elif alg == '512':
+            r = hashlib.sha512(text)
+        elif alg == '384':
+            r = hashlib.sha384(text)
+        # SHA-3 family
+        elif alg == '3256':
+            r = hashlib.sha3_256(text)
+        elif alg == '3224':
+            r = hashlib.sha3_224(text)
+        elif alg == '3512':
+            r = hashlib.sha3_512(text)
+        elif alg == '3384':
+            r = hashlib.sha3_384(text)
+        r = r.hexdigest()
+        self.result_sha.setText(r)
+
+
+
     def fcopy_fe(self):
         pc.copy(self.result_fe.text())
 
